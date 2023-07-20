13e1fdfe904106419b7bf042ceb34b1549bd8317
oscar <oscarmegodmg2002@gmail.com>
2023-07-20 18:34:40 -0500

    change in allowed hosts

--- testproject/settings.py
+++ testproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%%$vei$1v^c$458+tq7b*$c)^x96v4fqf5+6d^(4y#*kb5vsp$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
